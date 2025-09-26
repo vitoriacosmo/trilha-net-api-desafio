e994a7b44535ae45ee4b5b58c9f2f46c1f27f7b4
vitoriacosmo <vitoriacosmo@outlook.com>
2025-09-26 11:04:19 -0300

    criando o contexto

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
