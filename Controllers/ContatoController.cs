010dfae0e283a06bacfb626e984bc9d107d36288
vitoriacosmo <vitoriacosmo@outlook.com>
2025-09-26 16:15:45 -0300

    adição dos endpoints put e get

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -25,6 +25,34 @@ namespace ModuloAPI.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
 
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        { 
+         var contatoBanco = _context.Contatos.Find(id);
+           
+            if (contatoBanco == null)
+            {
+                return NotFound();
+            }
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            return Ok(contatoBanco);
+        }
     }
 }
